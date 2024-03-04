cbbda04123bf298419895372c96de01bc645e4f0
fabriccio27 <fabricio_inc27@hotmail.com>
2024-03-04 10:17:16 -0300

    Add config file for dev -cannot use this approach in builds, since they will be exposed

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:clima_updated/services/location.dart';
+import 'package:clima_updated/config_dev.dart';
+import 'package:http/http.dart' as http;
 
 
 class LoadingScreen extends StatefulWidget {
@@ -8,7 +12,8 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
+  late double latitude;
+  late double longitude;
 
   @override
   void initState() {
@@ -19,8 +24,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location locationService = Location();
     await locationService.getLocation();
-    print(locationService.latitude);
-    print(locationService.longitude);
+    latitude = locationService.latitude;
+    longitude = locationService.longitude;
+  }
+
+  void getWeatherData() async {
+    Uri url = Uri.https('api.weatherapi.com', '/v1/current.json', {
+      'key': Config.weatherApiKey,
+      'q': '${latitude.toString()},${longitude.toString()}',
+      'aqi': 'no',
+    });
+    http.Response response = await http.get(url);
+    String jsonData = response.body;
+    var decodedData = jsonDecode(jsonData);
+    var temperature = decodedData['current']['temp_c'];
+    var name = decodedData['location']['name'];
+    var condition = decodedData['condition']['code'];
   }
 
   @override
@@ -29,6 +48,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
+            getWeatherData();
           },
           child: const Text('Get Location'),
         ),
